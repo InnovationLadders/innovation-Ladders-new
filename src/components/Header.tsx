4ebb7f69e5d65e81c5c73483559e0bbd0437fea7
InnovationLadders <mansour@innovationladders.com>
2026-08-20 20:01:17 +0300

    Enlarge organization logo in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -83,7 +83,7 @@ export default function Header({ currentLang, setLang, currentPage, setCurrentPa
             <img
               src={logoImg}
               alt="Innovation Ladders Logo"
-              className="h-12 w-auto object-contain"
+              className="h-16 w-auto object-contain"
             />
           </div>
 
